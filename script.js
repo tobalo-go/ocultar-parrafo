7d52709c0f77d6855d2acbbbfddaa4719f541a5e
tobalo-go <tobalaguardia@gmail.com>
2025-03-10 09:15:39 +0100

    u

---
+++ script.js
@@ -0,0 +1,9 @@
+<script>
+        // Esperar a que el documento esté listo
+        $(document).ready(function() {
+            // Al hacer clic en el botón, ocultar el párrafo con una animación lenta
+            $("#ocultar-btn").click(function() {
+                $("#parrafo").hide("slow");
+            });
+        });
+    </script>
